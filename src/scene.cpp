4e6a336f1d57aa4cead72a565582bff8675479b2
Yue Zhu <u7442130@anu.edu.au>
2025-05-18 17:14:18 +1000

    First Commit For Scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,19 +1,62 @@
+#include "scene.hpp"
+#include "loadMesh.hpp"
+#include "json.hpp"
+#include "mesh.hpp"
+#include "shader.hpp"
+#include <fstream>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <map>
-#include "Eigen/Dense"
+#include <memory>
+#include <Eigen/Dense>
 
-#include <scene.hpp>
-#include <light.hpp>
-#include <object.hpp>
+using json = nlohmann::json;
+// Test if the json file can be loaded correctly
+void loadSceneFromJson(const std::string& path, Scene& scene) {
+    std::ifstream in(path);
+    if (!in.is_open()) {
+        std::cerr << "Failed to open scene JSON: " << path << std::endl;
+        return;
+    }
 
-using namespace std;
-using namespace Eigen;
+    json j;
+    in >> j;
 
-Scene buildScene(std::map<std::string, Object> objects){
-    Light dummyLight = Light(Vector3f(0, 10, 5), Vector3f(1, 1, 1));
-    Object dummyObject = objects.at("king");
-    return Scene({dummyObject}, {dummyLight}, {0, 0, 0});
+    for (const auto& obj : j["objects"]) {
+        std::string mesh_path = obj["mesh"];
+        std::shared_ptr<Mesh> mesh = loadModel(mesh_path);
+        if (!mesh) {
+            std::cerr << "Failed to load mesh from: " << mesh_path << std::endl;
+            continue;
+        }
+
+        // Default transform: identity
+        Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+
+        if (obj.contains("transform")) {
+            auto t = obj["transform"];
+            if (t.contains("translate")) {
+                Eigen::Vector3f tr(t["translate"][0], t["translate"][1], t["translate"][2]);
+                transform.translate(tr);
+            }
+            if (t.contains("scale")) {
+                Eigen::Vector3f s(t["scale"][0], t["scale"][1], t["scale"][2]);
+                transform.scale(s);
+            }
+            if (t.contains("rotate")) {
+                float angle = t["rotate"]["angle"];
+                Eigen::Vector3f axis(t["rotate"]["axis"][0], t["rotate"]["axis"][1], t["rotate"]["axis"][2]);
+                transform.rotate(Eigen::AngleAxisf(angle, axis.normalized()));
+            }
+        }
+
+        Material mat;
+        if (obj.contains("material")) {
+            auto m = obj["material"];
+            mat.ambient = Eigen::Vector3f(m["ambient"][0], m["ambient"][1], m["ambient"][2]);
+            mat.diffuse = Eigen::Vector3f(m["diffuse"][0], m["diffuse"][1], m["diffuse"][2]);
+            mat.specular = Eigen::Vector3f(m["specular"][0], m["specular"][1], m["specular"][2]);
+            mat.shininess = m["shininess"];
+        }
+
+        scene.addObject(mesh, mat, transform);
+    }
 }
